3d74e08b481710c230d08008a3001df596fea76c
Jerry W <jerrywiltse@gmail.com>
2017-09-09 00:47:09 -0400

    try fix build.py

--- build.py
+++ build.py
@@ -1,7 +1,10 @@
-from conan.packager import ConanMultiPackager, os
+from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing")
-    builder.add_common_builds()
+    builder = ConanMultiPackager()
+    builder.add({"arch": "Win32", "build_type": "Release"})
+    builder.add({"arch": "Win32", "build_type": "Debug"})
+    builder.add({"arch": "x64", "build_type": "Release"})
+    builder.add({"arch": "x64", "build_type": "Debug"})
     builder.run()
