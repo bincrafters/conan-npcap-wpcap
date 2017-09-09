4eefc334ff3c66a3b1e0c95bd6fd8771f84d941f
Jerry W <jerrywiltse@gmail.com>
2017-09-08 23:30:32 -0400

    add build missing

--- conanfile.py
+++ conanfile.py
@@ -9,20 +9,23 @@ class NpcapWpcapConan(ConanFile):
     source_url = "https://github.com/nmap/npcap"
     settings = "arch", "compiler", "build_type"  
     lib_parent_name = "npcap"
-    sln_path = os.path.join("wpcap", "libpcap", "Win32", "Prj")
-    sln_file = os.path.join(sln_path, "wpcap.sln")
-    proj_file = os.path.join(sln_path, "wpcap.vcxproj")
     build_requires = "npcap-dll/0.93@bincrafters/testing"
 
     def source(self):
         self.run("git clone --recursive --single-branch --depth 1 --branch=v{0} {1}.git".format(self.version, self.source_url)) 
     
     def build(self):
+        print("arch = " str(self.settings.arch))    
         vcvars = tools.vcvars_command(self.settings)
         self.run(vcvars)
+
+        sln_path = os.path.join("wpcap", "libpcap", "Win32", "Prj")
+        sln_file = os.path.join(sln_path, "wpcap.sln")
+        proj_file = os.path.join(sln_path, "wpcap.vcxproj")
+   
         unzip_dir = "{0}".format(self.lib_parent_name)
-        sln_path_full = os.path.join(unzip_dir, self.sln_file)
-        proj_path_full = os.path.join(unzip_dir, self.proj_file)
+        sln_path_full = os.path.join(unzip_dir, sln_file)
+        proj_path_full = os.path.join(unzip_dir, proj_file)
         npcap_dep = self.deps_cpp_info["npcap-dll"]
         npcap_lib_file = os.path.join(npcap_dep.rootpath, npcap_dep.libdirs[0], "Packet.lib")
         tools.replace_in_file(proj_path_full, r'..\..\..\..\packetWin7\Dll\Project\Release No NetMon and AirPcap\Packet.lib', npcap_lib_file)
@@ -38,7 +41,7 @@ class NpcapWpcapConan(ConanFile):
         self.copy("Win32-Extensions.h", dst="include", src=win32_ext_dir)
         
         arch_path = "x86" if self.settings.arch == "x86" else "x64"
-                
+        print("arch = " str(self.settings.arch))        
         lib_dir = os.path.join(libpcap_root, "Win32", "Prj", arch_path, str(self.settings.build_type))
         print(lib_dir)
         self.copy("wpcap.lib", dst="lib", src=lib_dir, keep_path=False)
