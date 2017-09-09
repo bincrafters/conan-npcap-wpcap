a42e06f3d507485b20195ae21b4694dedfda07f3
Jerry W <jerrywiltse@gmail.com>
2017-09-09 01:00:38 -0400

    fix build.py

--- build.py
+++ build.py
@@ -3,8 +3,8 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add({"arch": "Win32", "build_type": "Release"})
-    builder.add({"arch": "Win32", "build_type": "Debug"})
+    builder.add({"arch": "x86", "build_type": "Release"})
+    builder.add({"arch": "x86", "build_type": "Debug"})
     builder.add({"arch": "x64", "build_type": "Release"})
     builder.add({"arch": "x64", "build_type": "Debug"})
     builder.run()
