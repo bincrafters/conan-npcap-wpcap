65deb5c6f86e75ac25f6e76dc8be3f9e7008832d
Jerry W <jerrywiltse@gmail.com>
2017-09-09 01:04:14 -0400

    test_package add build missing

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(args="--build missing")
     builder.add({"arch": "x86", "build_type": "Release"})
     builder.add({"arch": "x86", "build_type": "Debug"})
     builder.add({"arch": "x64", "build_type": "Release"})
