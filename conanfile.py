5bc70814fef4c633e8409622f8701dbe38fd20a6
Jerry W <jerrywiltse@gmail.com>
2017-09-22 11:59:10 -0400

    add os import

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools
+from conans import ConanFile, tools, os
 from conans.tools import os_info, SystemPackageTool, ChocolateyTool
 
 class NpcapWpcapConan(ConanFile):
